9f708d3e3618757d3481750020ebe77cbca8a285
Jagoda Bieniek <jagoda.bieniek1234@gmail.com>
2026-03-27 11:24:11 +0100

    chore(): clean up

---  prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    arrowParens: 'always',
-    jsxSingleQuote: false,
-    bracketSpacing: true,
-    printWidth: 100,
-  };
